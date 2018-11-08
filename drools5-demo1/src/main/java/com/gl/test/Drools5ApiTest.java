8ff7417fc4f82e75f604b210bd17523f4fd1a844
gonglei20 <3345884708@qq.com>
2018-11-08 09:43:09 +0800

    drools5 api演示

--- drools5-demo1/src/main/java/com/gl/test/Drools5ApiTest.java
+++ drools5-demo1/src/main/java/com/gl/test/Drools5ApiTest.java
@@ -4,10 +4,15 @@ import com.gl.entity.Goods;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.Test;
 import org.kie.api.io.ResourceType;
-import org.kie.internal.KnowledgeBaseFactory;
+import org.kie.internal.KnowledgeBase;
 import org.kie.internal.builder.KnowledgeBuilder;
 import org.kie.internal.builder.KnowledgeBuilderFactory;
+import org.kie.internal.definition.KnowledgePackage;
 import org.kie.internal.io.ResourceFactory;
+import org.kie.internal.runtime.StatefulKnowledgeSession;
+
+import java.util.Collection;
+
 @Slf4j
 public class Drools5ApiTest {
 
@@ -15,11 +20,28 @@ public class Drools5ApiTest {
     public void drools5Test(){
 
         KnowledgeBuilder kbuilder = KnowledgeBuilderFactory.newKnowledgeBuilder();
-        kbuilder.add(ResourceFactory.newClassPathResource("com/gl/goods.drl",this.getClass()), ResourceType.DRL);
+        kbuilder.add(ResourceFactory.newClassPathResource("com/goods.drl",this.getClass()), ResourceType.DRL);
 
         if (kbuilder.hasErrors()){
-            log.error("drl文件内存在异常{}",kbuilder.getErrors());
+            //System.out.println("drl文件内存在异常"+kbuilder.getErrors());
+            log.error("drl文件内存在异常{}"+kbuilder.getErrors());
         }
+        KnowledgeBase knowledgeBase = kbuilder.newKnowledgeBase();
+        Collection<KnowledgePackage> knowledgePackages = kbuilder.getKnowledgePackages();
+        knowledgeBase.addKnowledgePackages(knowledgePackages);
+
+        //创建会话
+        StatefulKnowledgeSession statefulKnowledgeSession = knowledgeBase.newStatefulKnowledgeSession();
+
+        Goods gds = new Goods();
+        System.out.println("before "+gds.getDiscount());
+        //执行rule
+        statefulKnowledgeSession.insert(gds);
+        statefulKnowledgeSession.fireAllRules();
+
+        System.out.println("after "+gds.getDiscount());
+        //关闭资源
+        statefulKnowledgeSession.dispose();
 
 
     }
